ac30587404e546af47c07a34a7f7821a1946aa8d
hyacinth0213 <hyacinth0213@users.noreply.github.com>
2021-02-01 00:59:11 +0100

    创建了一个工具类

--- plot_spider/myPlotSpider/factory/hashtool.py
+++ plot_spider/myPlotSpider/factory/hashtool.py
@@ -1,6 +1,8 @@
 import hashlib
 
 
-def string_to_sha1_hash(s):  # 使用sha1将字符串转换成唯一的数值
-    m = hashlib.sha1(s.encode("utf-8")).hexdigest()
-    return m
+class Factories(object):  # 在这里加入一些静态类方法
+    @staticmethod
+    def string_to_sha1_hash(s):  # 使用sha1将字符串转换成唯一的数值
+        m = hashlib.sha1(s.encode("utf-8")).hexdigest()
+        return m
