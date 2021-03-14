b9220b4bf55fcf4479471c935effb493add8b18d
hyacinth0213 <hyacinth0213@users.noreply.github.com>
2021-03-14 22:06:33 +0100

    some changes

--- plot_spider/myPlotSpider/items.py
+++ plot_spider/myPlotSpider/items.py
@@ -6,20 +6,23 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Item
+from scrapy import Field
 
 
 class MyplotspiderItem(scrapy.Item):
+    """
+        @scrapy.item，继承父类scrapy.Item的属性和方法，该类用于定义需要爬取数据的子段
+    """
     # define the fields for your item here like:
-    bundesland = scrapy.Field()  # 联邦州名
-    county = scrapy.Field()  # 城市或地区名
-    title = scrapy.Field()  # 标题
-    property = scrapy.Field()  # 内容
-    uid = scrapy.Field()  # 唯一识别id
-    url = scrapy.Field()  # 链接地址
-    img_urls = scrapy.Field()  # 图片链接地址 (图片链接为一个list)
-    img_names = scrapy.Field()  # 图片名list ()
-    # pass
-    price = scrapy.Field()
+    article_bundesland = scrapy.Field()  # 联邦州名
+    article_county = scrapy.Field()  # 城市或地区名
+    article_title = scrapy.Field()  # 标题
+    article_content = scrapy.Field()  # 内容
+    article_uid = scrapy.Field()  # 唯一识别id
+    article_url = scrapy.Field()  # 链接地址
+    article_img_urls = scrapy.Field()  # 图片链接地址 (图片链接为一个list)
+    article_img_names = scrapy.Field()  # 图片名list ()
 
 
 class ImagesrenameItem(scrapy.Item):
--- plot_spider/myPlotSpider/spiders/brandenburg/altlandsberg.py
+++ plot_spider/myPlotSpider/spiders/brandenburg/altlandsberg.py
@@ -14,6 +14,9 @@ class AltlandsbergSpider(scrapy.Spider):
     start_urls = ['https://www.altlandsberg.de/index.php?page=grundstuecke']
 
     def parse(self, response):
+        """
+            @params:response,提取response中特定字段信息
+        """
         plot_list = response.xpath("//div[@class='item']")
         # len = 3
         # index = 0
@@ -35,6 +38,8 @@ class AltlandsbergSpider(scrapy.Spider):
             category = fix_field(plot_category_div.extract()[-1])
             properties = ''  # 在这里对之前提取的properties文本list对象进行处理
             properties = plot_properties_div_list  # 将描述的string列表直接传给properties变量
+            # 对properties内容进行处理
+
             # print("title: " + title + " ; category: " + category)
             # 提取image
             plot_properties_div = plot.xpath("div[@class='item-properties']")
@@ -42,14 +47,12 @@ class AltlandsbergSpider(scrapy.Spider):
             img_urls = plot_properties_div.xpath("a/img/@src").extract()
             url = response.request.url  # 获取链接地址
             # 提取pdf链接
-            # 将PDF文档定义为只有一个
-            # pdf_url = ''
             plot_link_list = plot_properties_div.xpath("a/@href")
-            # 一部分link是img，一部分link是pdf文档
-            # print(index)
+            # 一部分link是img, 一部分link是pdf文档, 对其进行分类
             # todo
-            # 在这里对其进行分类： 是文档还是图片？
-
+            item['article_bundesland'] = bundesland
+            item['article_county'] = county
+            item['article_title'] = title
         """
         # title = response.xpath("//div[@class='item']").xpath("//div[@class='item-title']//text()").extract_first()
         # '<div class="item-title">Gutshof Gielsdorf<br></div>'
